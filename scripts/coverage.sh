01ef732311a2fd8cdcbdc8f4e5a883ce3d00aa80
Luca Sepe <luca.sepe@gmail.com>
2025-03-27 08:47:01 +0100

    feat: add github workflow in order to run unit tests (#16)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-go test -count=1 -p 1 -cover -coverprofile=coverage.out ./...
-go tool cover -func=coverage.out
+go test -count=1 -p 1 -cover -coverprofile=coverage.txt ./...
+go tool cover -func=coverage.txt
